e53488ee3d66210aa9114724122f88ebba04464b
Manikandan B <xqsit94@gmail.com>
2026-01-09 13:44:19 +0530

    chore: update formula to v0.1.0-alpha with checksums

--- Formula/shelp.rb
+++ Formula/shelp.rb
@@ -7,7 +7,7 @@ class Shelp < Formula
   on_macos do
     on_intel do
       url "https://github.com/xqsit94/shelp/releases/download/v#{version}/shelp-darwin-amd64.tar.gz"
-      sha256 "PLACEHOLDER"
+      sha256 "632fc1a1876908d3ea0f4901be52be9c088dcc3bfe7f9ecc1ab28127ee5272f5"
 
       def install
         bin.install "shelp"
@@ -16,7 +16,7 @@ class Shelp < Formula
 
     on_arm do
       url "https://github.com/xqsit94/shelp/releases/download/v#{version}/shelp-darwin-arm64.tar.gz"
-      sha256 "PLACEHOLDER"
+      sha256 "2232602c8f7c6015e417d86871d8e70b7b8ec502f98c119ec4e769628cdf7610"
 
       def install
         bin.install "shelp"
@@ -27,7 +27,7 @@ class Shelp < Formula
   on_linux do
     on_intel do
       url "https://github.com/xqsit94/shelp/releases/download/v#{version}/shelp-linux-amd64.tar.gz"
-      sha256 "PLACEHOLDER"
+      sha256 "2e7bfce087b05ede95c8a99ddcadfb0fc8d0635d3faeb6529ebdb0565d32c9a8"
 
       def install
         bin.install "shelp"
@@ -36,7 +36,7 @@ class Shelp < Formula
 
     on_arm do
       url "https://github.com/xqsit94/shelp/releases/download/v#{version}/shelp-linux-arm64.tar.gz"
-      sha256 "PLACEHOLDER"
+      sha256 "d183fd0cf2c16f6be886bd63095fa3b3ea43a29e61700a1d60cd08c7b5137236"
 
       def install
         bin.install "shelp"
